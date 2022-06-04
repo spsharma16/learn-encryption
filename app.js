b263160028669145484c8f7203f20ade0da8e9da
Sp <spsharma38697@gmail.com>
2022-06-04 14:50:47 +0530

    added salting and hashing password with brcypt

--- app.js
+++ app.js
@@ -1,65 +1,75 @@
 //jshint esversion:6
 require('dotenv').config();
-const express=require("express");
+const express = require("express");
 const bodyParser = require("body-parser");
-const ejs =require("ejs");
-const mongoose=require("mongoose");
-const md5=require("md5");
-const app=express();
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
+
+const app = express();
 
 console.log(process.env.API);
 
 app.use(express.static("public"));
-app.set('view engine','ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
-const userSchema=new mongoose.Schema({
-    email:String,
-    password:String
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
 });
 
 
 
 
-const User=new mongoose.model("User",userSchema);
+const User = new mongoose.model("User", userSchema);
 
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
     res.render("home");
 });
-app.get("/login",function(req,res){
+app.get("/login", function (req, res) {
     res.render("login");
 });
-app.get("/register",function(req,res){
+app.get("/register", function (req, res) {
     res.render("register");
 });
 
-app.post("/register",function(req,res){
-    const newUser=new User({
-        email:req.body.username,
-         password:md5(req.body.password)
-    });
-    newUser.save(function(err){
-        if(err){
-          res.send(err);
-        }
-        else{
-            res.render("secrets");
-        }
+app.post("/register", function (req, res) {
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function (err) {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                res.render("secrets");
+            }
+        });
+
     });
 });
-app.post("/login",function(req,res){
-    const username=req.body.username;
-    const password=md5(req.body.password);
+app.post("/login", function (req, res) {
+    const username = req.body.username;
+    const password = (req.body.password);
 
-    User.findOne({email:username},function(err,founduser){
-        if(err){
+    User.findOne({ email: username }, function (err, founduser) {
+        if (err) {
             console.log(err);
         }
-        else{
-            if(founduser.password===password){
-            res.render("secrets");
+        else {
+            if (founduser) {
+                bcrypt.compare(password, founduser.password, function (err, result) {
+                    if (result === true) {
+                        res.render("secrets");
+
+                    }
+                });
             }
 
         }
@@ -67,6 +77,6 @@ app.post("/login",function(req,res){
     });
 });
 
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
